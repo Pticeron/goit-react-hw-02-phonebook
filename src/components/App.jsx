9d3ca059c341b5704edcdd3f9ebdbdd496a68efb
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-04-29 18:39:51 +0300

    add filter and fix app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,27 +12,36 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    // filter: '',
+    filter: '',
     name: '',
     number: '',
   
   };
 
 handleChange = e => {
-  console.log(e);
     const { name, value } = e.target;
     this.setState({ [name]: value });
 };
 
 handleSubmit = e => {
-  console.log(e);
   const id = nanoid();
   const name = e.name;
   const number = e.number;
   const contactsLists = [...this.state.contacts];
-  contactsLists.push({ name, id, number });
+  
+  if (contactsLists.findIndex(contact => name === contact.name) !== -1) {
+    alert(`${name} is already in contacts.`);
+  } else {
+    contactsLists.push({ name, id, number });
+  }
 
   this.setState({ contacts: contactsLists });
+};
+
+handleDelete = e => {
+  this.setState(prevState => ({
+    contacts: prevState.contacts.filter(contact => contact.id !== e),
+  }));
 };
 
 getFilteredContacts = () => {
@@ -41,6 +50,7 @@ getFilteredContacts = () => {
       .toLowerCase()
       .includes(this.state.filter.toLowerCase());
   });
+
   return filterContactsList;
   };
 
@@ -52,28 +62,24 @@ render() {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 20,
-        color: '#010101',
-      }}
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+        }}
     >
       <h1>Phonebook</h1>
       <ContactForm handleSubmit={this.handleSubmit} />
       <h2> Contacts</h2>
       <Filter filter={filter} handleChange={this.handleChange} />
-      <ContactList contacts={this.getFilteredContacts()} />
+      <ContactList
+        contacts={this.getFilteredContacts()} 
+        handleDelete={this.handleDelete}
+        />
     </div>
   );
 }
 }
-/* <div>
-  <h1>Phonebook</h1>
-  <ContactForm ... />
-
-  <h2>Contacts</h2>
-  <Filter ... />
-  <ContactList ... />
-</div> */
